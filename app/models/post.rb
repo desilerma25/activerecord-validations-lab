7ee2b82652bcffe8f8c4b0dc3a6f9f0a7b8be94c
Desiree Lerma <desi.lerma96@yahoo.com>
2020-12-22 19:27:15 -0600

    commit tree 4e0cd8f59c1735702715851b9ead63f0f0d38846

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    # category is fiction/nonfiction, requires inclusion validator
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+    # add custom validator, ensures title contains: "Won't Believe", "Secret", "Top [number]", or "Guess"
+    validate :title_clickbait?
+
+    def title_clickbait?
+        if title.present? && title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+        else
+            errors.add(:title, "is not clickbaity")
+        end
+    end
 end
